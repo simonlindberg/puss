7d1e3529b9a7203a4900c406c44539928025e688
ada10vby <ada10vby@student.lu.se>
2013-10-15 15:05:58 +0200

    Fixed Activity methods and finished test for TimeReport

--- ProjectPUSS/src/test/TimeReportTest.java
+++ ProjectPUSS/src/test/TimeReportTest.java
@@ -31,21 +31,21 @@ public class TimeReportTest {
 		user = new User("test", "password");
 		activities = new ArrayList<Activity>();
 		Activity ac;
-		ac = new Activity(ActivityType.SDP, 0);
+		ac = new Activity(ActivityType.SDP, 10);
+		activities.add(0, ac);
+		ac = new Activity(ActivityType.SRS, 20);
 		activities.add(ac);
-		ac = new Activity(ActivityType.SRS, 0);
+		ac = new Activity(ActivityType.SVVS, 30);
 		activities.add(ac);
-		ac = new Activity(ActivityType.SVVS, 0);
+		ac = new Activity(ActivityType.STLDD, 400);
 		activities.add(ac);
-		ac = new Activity(ActivityType.STLDD, 0);
+		ac = new Activity(ActivityType.SVVI, 500);
 		activities.add(ac);
-		ac = new Activity(ActivityType.SVVI, 0);
+		ac = new Activity(ActivityType.SDDD, 600);
 		activities.add(ac);
-		ac = new Activity(ActivityType.SDDD, 0);
+		ac = new Activity(ActivityType.SVVR, 700);
 		activities.add(ac);
-		ac = new Activity(ActivityType.SVVR, 0);
-		activities.add(ac);
-		ac = new Activity(ActivityType.SSD, 0);
+		ac = new Activity(ActivityType.SSD, 800);
 		activities.add(ac);
 		
 		tr1 = new TimeReport(user, activities, true, 0, 1, "group1");
@@ -78,5 +78,48 @@ public class TimeReportTest {
 		assertEquals("group1", tr1.getProjectGroup());
 		assertEquals("group2", tr2.getProjectGroup());
 	}
+	@Test
+	public void testUser() {
+		assertEquals("test", tr1.getUser().getUsername());
+	}
+	@Test
+	public void testReportId() {
+		assertEquals(0, tr1.getID());
+		assertEquals(1, tr2.getID());
+	}
+	@Test
+	public void testActivities() {
+		int i = 0;
+		assertEquals(ActivityType.SDP, tr1.getActivities().get(i).getType());
+		assertEquals(10, tr1.getActivities().get(i).getLength());
+		
+		i++;
+		assertEquals(ActivityType.SRS, tr1.getActivities().get(i).getType());
+		assertEquals(20, tr1.getActivities().get(i).getLength());
+		
+		i++;
+		assertEquals(ActivityType.SVVS, tr1.getActivities().get(i).getType());
+		assertEquals(30, tr1.getActivities().get(i).getLength());
+		
+		i++;
+		assertEquals(ActivityType.STLDD, tr1.getActivities().get(i).getType());
+		assertEquals(400, tr1.getActivities().get(i).getLength());
+		
+		i++;
+		assertEquals(ActivityType.SVVI, tr1.getActivities().get(i).getType());
+		assertEquals(500, tr1.getActivities().get(i).getLength());
+		
+		i++;
+		assertEquals(ActivityType.SDDD, tr1.getActivities().get(i).getType());
+		assertEquals(600, tr1.getActivities().get(i).getLength());
+		
+		i++;
+		assertEquals(ActivityType.SVVR, tr1.getActivities().get(i).getType());
+		assertEquals(700, tr1.getActivities().get(i).getLength());
+		
+		i++;
+		assertEquals(ActivityType.SSD, tr1.getActivities().get(i).getType());
+		assertEquals(800, tr1.getActivities().get(i).getLength());
 
+	}
 }
