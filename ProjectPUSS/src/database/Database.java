e6d56f300b6eb71c670159f94bac5e43c0160a96
ericottosson <ada10eot@student.lu.se>
2013-10-16 15:34:16 +0200

    You can now delete users from projectgroups.

--- ProjectPUSS/src/database/Database.java
+++ ProjectPUSS/src/database/Database.java
@@ -365,6 +365,14 @@ public class Database {
 	 * 
 	 */
 	public boolean deleteUserFromProject(String projectName, String userName) {
+		try {
+			return conn.createStatement().execute(
+					"delete from Memberships where Groupname='" + projectName + "' AND Username='" + userName + "'");
+		} catch (SQLException e) {
+			System.out.println("");
+			e.printStackTrace();
+		}
+
 		return false;
 	}
 
