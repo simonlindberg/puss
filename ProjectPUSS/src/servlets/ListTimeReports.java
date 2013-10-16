a31bc20d1da2a01cefb52032a0e63aa5315d698c
Nicklas Erman <nicklas.erman@gmail.com>
2013-10-16 09:39:19 +0200

    Added catch statement

--- ProjectPUSS/src/servlets/ListTimeReports.java
+++ ProjectPUSS/src/servlets/ListTimeReports.java
@@ -42,6 +42,9 @@ public class ListTimeReports extends ServletBase {
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			} 
 			
 		}
